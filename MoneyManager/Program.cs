914bacee4dbaa8e2bdb4691b2980dc3cf376ee4c
Rúben Lopes <rubenlopespro@gmail.com>
2025-03-05 16:28:26 +0000

    feat: Program connection to DataBase on Azure

--- MoneyManager/Program.cs
+++ MoneyManager/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.Extensions.Options;
 using MoneyManager.Data.Interface;
 using MoneyManager.Data.Services;
+using MoneyManager.Shared;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,29 @@ builder.Services.AddScoped<IMonthSheet, MonthSheetService>();
 builder.Services.AddScoped<ICategory, CategoryService>();
 builder.Services.AddScoped<ISeller, SellerService>();
 builder.Services.AddScoped<IAccount, AccountService>();
+
+#region Connect with DB
+// Load MongoDB settings from configuration
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDB"));
+
+// Register MongoDB client as a singleton service
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    // Check if the environment variable is set (for production)
+    var envConn = Environment.GetEnvironmentVariable("MONGO_CLUSTER_CONNECTION_STRING");
+    if (!string.IsNullOrEmpty(envConn))
+    {
+        settings.ConnectionString = envConn;
+    }
+    return new MongoClient(settings.ConnectionString);
+});
+#endregion
 
 var app = builder.Build();
 

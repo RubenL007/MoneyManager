69cc29dd376586e7bb901bc59e6c0c2c8c2d76e0
Rúben Lopes <rubenlopespro@gmail.com>
2025-06-14 16:35:52 +0100

    feat: ColorHelper and ColorsPercentageEnum, to get a tint or shade of a color

---
+++ MoneyManager/Shared/Components/Colors/ColorHelper.cs
@@ -0,0 +1,12 @@
+﻿using MoneyManager.Shared.Components.Colors;
+
+namespace MoneyManager.Shared.Components
+{
+    public class ColorHelper
+    {
+        public static string ColorEnumToHTML(ColorsEnum colorByEnum, ColorsPercentageEnum? colorPercentage = null)
+        {
+            return $"var(--{colorByEnum}{(colorPercentage != null ? $"-{(int)colorPercentage}" : string.Empty)})";
+        }
+    }
+}
---
+++ MoneyManager/Shared/Components/Colors/ColorsPercentageEnum.cs
@@ -0,0 +1,17 @@
+﻿namespace MoneyManager.Shared.Components.Colors
+{
+    public enum ColorsPercentageEnum
+    {
+        Per10 = 10,
+        Per50 = 50,
+        Per100 = 100,
+        Per200 = 200,
+        Per300 = 300,
+        Per400 = 400,
+        PerDefault = 500,
+        Per600 = 600,
+        Per700 = 700,
+        Per800 = 800,
+        Per900 = 900,
+    }
+}

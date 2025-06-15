adf6bbeb7bfc4a35e7358acdde6b6fe52b2b3453
Rúben Lopes <rubenlopespro@gmail.com>
2025-06-15 15:18:55 +0100

    feat: RandomHexColor method to ColorHelper

--- MoneyManager/Shared/Components/Colors/ColorHelper.cs
+++ MoneyManager/Shared/Components/Colors/ColorHelper.cs
@@ -8,5 +8,16 @@ namespace MoneyManager.Shared.Components
         {
             return $"var(--{colorByEnum}{(colorPercentage != null ? $"-{(int)colorPercentage}" : string.Empty)})";
         }
+
+        public static string RandomHexColor()
+        {
+            var random = new Random();
+
+            int r = random.Next(100, 256);
+            int g = random.Next(100, 256);
+            int b = random.Next(100, 256);
+
+            return $"#{r:X2}{g:X2}{b:X2}";
+        }
     }
 }

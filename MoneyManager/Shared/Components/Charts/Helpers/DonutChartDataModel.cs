848bff80d4d6d0e2d0e35f58ed3bd5897890532a
Rúben Lopes <rubenlopespro@gmail.com>
2025-06-22 12:26:52 +0100

    feat(comp): DonutChart

---
+++ MoneyManager/Shared/Components/Charts/Helpers/DonutChartDataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MoneyManager.Shared.Components
+{
+    public class DonutChartDataModel
+    {
+        public double Value { get; set; } = 0;
+        public string Label { get; set; } = string.Empty;
+
+        public string ColorFill { get; set; } = string.Empty;
+    }
+}
